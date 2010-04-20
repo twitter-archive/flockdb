5c48380b224a7a10cabda589cf1ab2ed3f685bb1
Robey Pointer <robey@twitter.com>
2010-04-20 12:06:33 -0700

    this is messing up TestRunner.

--- src/test/scala/com/twitter/flockdb/ConfiguredSpecification.scala
+++ src/test/scala/com/twitter/flockdb/ConfiguredSpecification.scala
@@ -4,7 +4,7 @@ import net.lag.configgy.Configgy
 import org.specs.Specification
 
 abstract class ConfiguredSpecification extends Specification {
-  Configgy.configure("config/test.conf")
-  val config = Configgy.config
+//  Configgy.configure("config/test.conf")
+  lazy val config = Configgy.config
 }
 
