d349f8e5a478d9e273568f101125613b08f07c95
Robey Pointer <robey@twitter.com>
2010-04-27 16:59:38 -0700

    add apache license headers.

--- src/scripts/setup-env.sh
+++ src/scripts/setup-env.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
+#
+# Copyright 2010 Twitter, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 if java -version 2>&1 |grep "1\.5"; then
   echo "Java must be at least 1.6"
