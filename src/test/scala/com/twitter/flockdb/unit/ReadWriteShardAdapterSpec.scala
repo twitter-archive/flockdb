bc58b5e554494fb6b27d0eb29096aab78a1747de
Robey Pointer <robey@twitter.com>
2010-09-03 21:04:11 -0700

    add a test for coalescing metadata.

--- src/test/scala/com/twitter/flockdb/unit/ReadWriteShardAdapterSpec.scala
+++ src/test/scala/com/twitter/flockdb/unit/ReadWriteShardAdapterSpec.scala
@@ -16,9 +16,10 @@
 
 package com.twitter.flockdb.unit
 
-import com.twitter.gizzard.shards.{ReadWriteShard, ShardInfo}
+import com.twitter.gizzard.shards.{ReadWriteShard, ReplicatingShard, ShardInfo}
 import com.twitter.gizzard.test.FakeReadWriteShard
 import com.twitter.xrayspecs.Time
+import com.twitter.xrayspecs.TimeConversions._
 import org.specs.mock.{ClassMocker, JMocker}
 import shards.{ReadWriteShardAdapter, Shard, Metadata}
 import thrift.Edge
@@ -51,6 +52,43 @@ object ReadWriteShardAdapterSpec extends ConfiguredSpecification with JMocker wi
       val shard = new ReadWriteShardAdapter(fake1)
       shard.withLock(sourceId) { (innerShard, metadata) =>
         innerShard.add(sourceId, Time.now)
+      }
+    }
+
+    "coalesce metadata" in {
+      val child1 = mock[Shard]
+      val child2 = mock[Shard]
+      val child3 = mock[Shard]
+      val children = List(child1, child2, child3)
+      val fake1 = new ReplicatingShard[Shard](null, 1, children, () => children, null)
+      val shard = new ReadWriteShardAdapter(fake1)
+
+      "with a clear time winner" in {
+        val metadata1 = Metadata(3, State.Removed, 1, Time(3.seconds))
+        val metadata2 = Metadata(3, State.Archived, 1, Time(3.seconds))
+        val metadata3 = Metadata(3, State.Normal, 1, Time(4.seconds))
+
+        expect {
+          one(child1).getMetadata(3) willReturn Some(metadata1)
+          one(child2).getMetadata(3) willReturn Some(metadata2)
+          one(child3).getMetadata(3) willReturn Some(metadata3)
+        }
+
+        shard.getMetadata(3) mustEqual Some(metadata3)
+      }
+
+      "with the same time" in {
+        val metadata1 = Metadata(3, State.Removed, 1, Time(3.seconds))
+        val metadata2 = Metadata(3, State.Archived, 1, Time(3.seconds))
+        val metadata3 = Metadata(3, State.Normal, 1, Time(3.seconds))
+
+        expect {
+          one(child1).getMetadata(3) willReturn Some(metadata1)
+          one(child2).getMetadata(3) willReturn Some(metadata2)
+          one(child3).getMetadata(3) willReturn Some(metadata3)
+        }
+
+        shard.getMetadata(3) mustEqual Some(metadata1)
       }
     }
   }
