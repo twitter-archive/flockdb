01eb8d40b4a1356b544e7edbe9d44000a5b1130d
Robey Pointer <robey@twitter.com>
2010-09-03 20:22:01 -0700

    withOptimisticLock.

--- src/main/scala/com/twitter/flockdb/jobs/single/Single.scala
+++ src/main/scala/com/twitter/flockdb/jobs/single/Single.scala
@@ -16,6 +16,7 @@
 
 package com.twitter.flockdb.jobs.single
 
+import com.twitter.flockdb.shards.Shard
 import com.twitter.gizzard.jobs.{UnboundJobParser, UnboundJob}
 import com.twitter.xrayspecs.Time
 import com.twitter.xrayspecs.TimeConversions._
@@ -71,27 +72,33 @@ abstract class Single(sourceId: Long, graphId: Int, destinationId: Long, positio
     (forwardShard, backwardShard)
   }
 
+  private def withOptimisticLock(forwardShard: Shard, backwardShard: Shard, sourceId: Long, destinationId: Long)(f: State => Unit) {
+    val initialState = forwardShard.getMetadata(sourceId).map(_.state).getOrElse(State.Normal) max backwardShard.getMetadata(destinationId).map(_.state).getOrElse(State.Normal)
+    f(initialState)
+    val endState = forwardShard.getMetadata(sourceId).map(_.state).getOrElse(State.Normal) max backwardShard.getMetadata(destinationId).map(_.state).getOrElse(State.Normal)
+    if (endState != initialState) {
+      throw new Exception("Lost optimistic lock on id " + sourceId + " / " + destinationId)
+    }
+  }
+
   def apply(environment: (ForwardingManager, UuidGenerator)) {
     val (forwardingManager, uuidGenerator) = environment
     val (forwardShard, backwardShard) = shards(forwardingManager)
     val uuid = uuidGenerator(position)
-    forwardShard.withLock(sourceId) { (forwardShard, forwardMetadata) =>
-      backwardShard.withLock(destinationId) { (backwardShard, backwardMetadata) =>
-        val finalState = forwardMetadata.state max backwardMetadata.state max preferredState
-        finalState match {
-          case State.Normal =>
-            forwardShard.add(sourceId, destinationId, uuid, updatedAt)
-            backwardShard.add(destinationId, sourceId, uuid, updatedAt)
-          case State.Removed =>
-            forwardShard.remove(sourceId, destinationId, uuid, updatedAt)
-            backwardShard.remove(destinationId, sourceId, uuid, updatedAt)
-          case State.Archived =>
-            forwardShard.archive(sourceId, destinationId, uuid, updatedAt)
-            backwardShard.archive(destinationId, sourceId, uuid, updatedAt)
-          case State.Negative =>
-            forwardShard.negate(sourceId, destinationId, uuid, updatedAt)
-            backwardShard.negate(destinationId, sourceId, uuid, updatedAt)
-        }
+    withOptimisticLock(forwardShard, backwardShard, sourceId, destinationId) { initialState =>
+      (initialState max preferredState) match {
+        case State.Normal =>
+          forwardShard.add(sourceId, destinationId, uuid, updatedAt)
+          backwardShard.add(destinationId, sourceId, uuid, updatedAt)
+        case State.Removed =>
+          forwardShard.remove(sourceId, destinationId, uuid, updatedAt)
+          backwardShard.remove(destinationId, sourceId, uuid, updatedAt)
+        case State.Archived =>
+          forwardShard.archive(sourceId, destinationId, uuid, updatedAt)
+          backwardShard.archive(destinationId, sourceId, uuid, updatedAt)
+        case State.Negative =>
+          forwardShard.negate(sourceId, destinationId, uuid, updatedAt)
+          backwardShard.negate(destinationId, sourceId, uuid, updatedAt)
       }
     }
   }
