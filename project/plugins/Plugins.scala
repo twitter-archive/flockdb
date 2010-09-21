c1db2c6a9d28f3629810cf0ce8f112cfdaa6632d
Robey Pointer <robey@twitter.com>
2010-09-21 15:01:32 -0700

    update plugins.

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,7 +1,6 @@
 import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  val scalaTools = "scala-tools.org" at "http://scala-tools.org/repo-releases/"
-  val lagNet = "twitter.com" at "http://www.lag.net/repo/"
-  val defaultProject = "com.twitter" % "standard-project" % "0.7.1"
+  val twitterMaven = "twitter.com" at "http://maven.twttr.com/"
+  val defaultProject = "com.twitter" % "standard-project" % "0.7.5"
 }
