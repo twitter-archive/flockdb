88d46a60ca838bd5f3cbe177b93ee3ff316c5ebc
Kyle Maxwell <kmaxwell@twitter.com>
2010-12-01 11:14:00 -0800

    inline

--- project/build/FlockDBProject.scala
+++ project/build/FlockDBProject.scala
@@ -2,8 +2,8 @@ import sbt._
 import Process._
 import com.twitter.sbt._
 
-class FlockDBProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher {
-  val gizzard   = "com.twitter" % "gizzard" % "1.4.7"
+class FlockDBProject(info: ProjectInfo) extends StandardProject(info) with SubversionPublisher with InlineDependencies {
+  inline("com.twitter" % "gizzard" % "1.4.9-SNAPSHOT")
   val results   = "com.twitter" % "results" % "1.0"
 
   val asm       = "asm" % "asm" %  "1.5.3" % "test"
