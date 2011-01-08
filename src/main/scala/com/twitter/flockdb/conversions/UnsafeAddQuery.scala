74ee5f93146e53847eb53c4ab98fc4fbb47b68f7
eaceaser <eac@twitter.com>
2011-01-07 19:33:29 -0800

    use seconds like everything else for the thrift interface

--- src/main/scala/com/twitter/flockdb/conversions/UnsafeAddQuery.scala
+++ src/main/scala/com/twitter/flockdb/conversions/UnsafeAddQuery.scala
@@ -6,12 +6,12 @@ import com.twitter.util.TimeConversions._
 
 object UnsafeAddQuery {
   class RichFlockUnsafeAddQuery(term: flockdb.UnsafeAddQuery) {
-    def toThrift = new thrift.UnsafeAddQuery(term.sourceId, term.graphId, term.destinationId, term.createdAt.inMillis)
+    def toThrift = new thrift.UnsafeAddQuery(term.sourceId, term.graphId, term.destinationId, term.createdAt.inSeconds)
   }
   implicit def richFlockUnsafeAddQuery(term: flockdb.UnsafeAddQuery) = new RichFlockUnsafeAddQuery(term)
 
   class RichThriftUnsafeAddQuery(term: thrift.UnsafeAddQuery) {
-    def fromThrift = new flockdb.UnsafeAddQuery(term.source_id, term.graph_id, term.destination_id, Time(term.created_at.millis))
+    def fromThrift = new flockdb.UnsafeAddQuery(term.source_id, term.graph_id, term.destination_id, Time(term.created_at.seconds))
   }
   implicit def richThriftUnsafeAddQuery(term: thrift.UnsafeAddQuery) = new RichThriftUnsafeAddQuery(term)
 }
