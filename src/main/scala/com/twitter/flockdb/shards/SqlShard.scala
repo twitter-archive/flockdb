7fa4fa480450f6a666d3dcaaa1da7959a9c37fa0
Matt Freels <matt@freels.name>
2011-01-07 16:33:11 -0800

    Revert "try using plain insert and catch the constraint violation when initializing 1 row of metadata"
    This reverts commit 094b1343e3d3e5eacc2291c42085eaf4582aaea4.

--- src/main/scala/com/twitter/flockdb/shards/SqlShard.scala
+++ src/main/scala/com/twitter/flockdb/shards/SqlShard.scala
@@ -192,6 +192,12 @@ class SqlShard(val queryEvaluator: QueryEvaluator, val shardInfo: shards.ShardIn
     (edges, returnedCursor)
   }
 
+  private def existingMetadata(ids: Collection[Long]): Seq[Long] = {
+    queryEvaluator.select("SELECT source_id FROM " + tablePrefix + "_metadata WHERE source_id IN (?)", ids.toList) { row =>
+      row.getLong("source_id")
+    }
+  }
+
   private def existingEdges(edges: Collection[Edge]) = {
     val where = edges.map{edge => "(source_id = " + edge.sourceId + " AND destination_id=" +edge.destinationId+")"}.mkString(" OR ")
     val query = "SELECT source_id, destination_id FROM " + tablePrefix + "_edges WHERE " + where
@@ -206,20 +212,11 @@ class SqlShard(val queryEvaluator: QueryEvaluator, val shardInfo: shards.ShardIn
   private def statePriority(state: String): String = "-IF(" + state + "=0, 4, " + state + ")"
 
   private def initializeMetadata(queryEvaluator: QueryEvaluator, sourceIds: Set[Long]): Unit = {
-    if (!sourceIds.isEmpty) {
-      val values = sourceIds.map("("+ _ +", 0,0,0)").mkString(",")
-
-      val query = if (sourceIds.size == 1) {
-        "INSERT INTO " + tablePrefix + "_metadata (source_id, count, state, updated_at) VALUES " + values
-      } else {
-        "INSERT IGNORE INTO " + tablePrefix + "_metadata (source_id, count, state, updated_at) VALUES " + values
-      }
-
-      try {
-        queryEvaluator.execute(query)
-      } catch {
-        case e: SQLIntegrityConstraintViolationException => () // ignore duplicate key exception
-      }
+    val newIds = sourceIds -- existingMetadata(sourceIds)
+    if (!newIds.isEmpty) {
+      val values = newIds.map("(" + _ + ")").mkString(",")
+      val query = "INSERT IGNORE INTO " + tablePrefix + "_metadata (source_id) VALUES " + values
+      queryEvaluator.execute(query)
     }
   }
 
@@ -256,7 +253,7 @@ class SqlShard(val queryEvaluator: QueryEvaluator, val shardInfo: shards.ShardIn
           // Initialize metadata
           val sourceIdSet = Set(edges.map(_.sourceId): _*)
           val metaInitTime = time {
-            initializeMetadata(queryEvaluator, sourceIdSet)
+            initializeMetadata(transaction, sourceIdSet)
           }
           log.info("init metadata ("+ sourceIdSet.size +" rows) elapsed millis: "+ metaInitTime.inMilliseconds)
 
