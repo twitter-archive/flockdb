4cfad6ffc48dacf450295e9b4781fb2c5769a703
Kyle Maxwell <kmaxwell@twitter.com>
2011-01-14 18:25:10 -0800

    more docs

--- src/main/scala/com/twitter/flockdb/shards/Optimism.scala
+++ src/main/scala/com/twitter/flockdb/shards/Optimism.scala
@@ -28,6 +28,28 @@ class OptimisticLockException(message: String) extends ShardException(message)
  *    after the row is written.  The key case to worry about here is when the consensus looks like
  *    Seq(old_row, Offline(new_row)).  In this case, the non-unanimous consensus would write the
  *    wrong color.  We require that the consensus be unanimous for this reason.
+ *
+ * The current iteration of the optimism branch is (perhaps overly) conservative by maintaining
+ * a lock over all replicas. We could choose to go a couple different ways from here:
+ *
+ * 1. Hold the course.  This works.
+ * 2. Only lock over (any) one server of each replication set (so two total--one forwards, one
+ *    backwards).  This would reduce the number of reads.  If our data is (a) eventually-but-not-
+ *    yet-consistent and not just corrupted, and (b) two copies of a node have different states,
+ *    then the job to update the state of the node to the newer state is still active.  If we write
+ *    the wrong edge color based upon the old node, it is true that there is a multi job in some
+ *    presently existing queue that will fix the edge.
+ * 3. Do read repair.  When we do the optimistic lock read over all replicas, we're already grabbing
+ *    all of the information we need to repair the graphs.  Instead of just throwing an exception,
+ *    or ignoring the inconsistency, we could enqueue the appropriate multi job to perform the repair.
+ *    My chief concern would be that we could issue a storm of multi jobs.
+ *
+ * Eventually, we will probably choose (2) or (3), depending on how we weigh the tradeoff between
+ * repairing consistency and raw performance.  However, we should stick to (1) for now, and evaluate
+ * the other options soon.
+ *
+ * Also, in the short term, it's worth understanding (2), so that you can realize that adding
+ * replicas doesn't screw things up.
  */
 trait Optimism extends Shard {
   protected case class MetadataWithEx(metadata: Metadata, ex: Option[Throwable])
