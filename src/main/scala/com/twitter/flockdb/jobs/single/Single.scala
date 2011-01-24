7adbaf723855d156339d089b305bae44f844da00
Kyle Maxwell <kmaxwell@twitter.com>
2011-01-24 10:42:10 -0800

    fix black hole regression

--- src/main/scala/com/twitter/flockdb/jobs/single/Single.scala
+++ src/main/scala/com/twitter/flockdb/jobs/single/Single.scala
@@ -77,10 +77,19 @@ abstract class Single(sourceId: Long, graphId: Int, destinationId: Long, positio
     val forwardShard = forwardingManager.find(sourceId, graphId, Direction.Forward)
     val backwardShard = forwardingManager.find(destinationId, graphId, Direction.Backward)
     val uuid = uuidGenerator(position)
-    forwardShard.optimistically(sourceId) { left =>
-      backwardShard.optimistically(destinationId) { right =>
+    optimistically(forwardShard, sourceId) { left =>
+      optimistically(backwardShard, destinationId) { right =>
         write(forwardShard, backwardShard, uuid, left max right max preferredState)
       }
+    }
+  }
+
+  def optimistically(shard: Shard, id: Long)(f: (State => Unit)) = {
+    try {
+      shard.optimistically(id)(f)
+    } catch {
+      case e: ShardBlackHoleException =>
+        f(State.Normal)
     }
   }
 
