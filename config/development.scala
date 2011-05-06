4c2f2552c91d6a1cd5b9113ce5f9001269511abf
Chris Chandler <squanderingtime@gmail.com>
2011-05-06 17:44:37 -0500

    Moved query.timeouts from the ProductionNameServerReplica's ProductionQueryEvaluator to the actual ProductionQueryEvaluator declaration.

--- config/development.scala
+++ config/development.scala
@@ -35,6 +35,13 @@ class ProductionQueryEvaluator extends QueryEvaluator {
     poolSize = 10
     queueSize = 10000
   }
+  
+  query.timeouts = Map(
+    QueryClass.Select -> QueryTimeout(1.second),
+    QueryClass.Execute -> QueryTimeout(1.second),
+    QueryClass.SelectCopy -> QueryTimeout(15.seconds),
+    QueryClass.SelectModify -> QueryTimeout(3.seconds)
+  )
 }
 
 class ProductionNameServerReplica(host: String) extends Mysql {
@@ -53,13 +60,6 @@ class ProductionNameServerReplica(host: String) extends Mysql {
     database.timeout.foreach { t =>
       t.open = 1.second
     }
-
-    query.timeouts = Map(
-      QueryClass.Select -> QueryTimeout(1.second),
-      QueryClass.Execute -> QueryTimeout(1.second),
-      QueryClass.SelectCopy -> QueryTimeout(15.seconds),
-      QueryClass.SelectModify -> QueryTimeout(3.seconds)
-    )
   }
 }
 
