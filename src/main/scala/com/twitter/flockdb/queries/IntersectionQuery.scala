6c62d62285f55011751b2eb61e55d741ffd16c32
eaceaser <eac@twitter.com>
2011-08-10 15:52:13 -0700

    Revert "get rid of "optimization" that was breaking 2nd order intersections"
    This reverts commit 8b01c301c3ba2004f9f724becabc80ce0edff047.

--- src/main/scala/com/twitter/flockdb/queries/IntersectionQuery.scala
+++ src/main/scala/com/twitter/flockdb/queries/IntersectionQuery.scala
@@ -37,19 +37,23 @@ class IntersectionQuery(query1: QueryTree, query2: QueryTree, averageIntersectio
   def selectPage(count: Int, cursor: Cursor) = selectPageByDestinationId(count, cursor)
 
   def selectPageByDestinationId(count: Int, cursor: Cursor) = time({
-    val guessedPageSize = (count / averageIntersectionProportion).toInt
-    val internalPageSize = guessedPageSize min intersectionPageSizeMax.toInt
-    val timeout = intersectionTimeout.inMillis
+    if (count1 == 0 || count2 == 0) {
+      new ResultWindow(List[(Long,Cursor)](), count, cursor)
+    } else {
+      val guessedPageSize = (count / averageIntersectionProportion).toInt
+      val internalPageSize = guessedPageSize min intersectionPageSizeMax.toInt
+      val timeout = intersectionTimeout.inMillis
 
-    val now = System.currentTimeMillis
-    var resultWindow = pageIntersection(smallerQuery, largerQuery, internalPageSize, count, cursor)
-    while (resultWindow.page.size < count &&
-           resultWindow.continueCursor != Cursor.End &&
-           System.currentTimeMillis - now < timeout
-    ) {
-      resultWindow = resultWindow ++ pageIntersection(smallerQuery, largerQuery, internalPageSize, count, resultWindow.continueCursor)
+      val now = System.currentTimeMillis
+      var resultWindow = pageIntersection(smallerQuery, largerQuery, internalPageSize, count, cursor)
+      while (resultWindow.page.size < count &&
+             resultWindow.continueCursor != Cursor.End &&
+             System.currentTimeMillis - now < timeout
+      ) {
+        resultWindow = resultWindow ++ pageIntersection(smallerQuery, largerQuery, internalPageSize, count, resultWindow.continueCursor)
+      }
+      resultWindow
     }
-    resultWindow
   })
 
   def selectWhereIn(page: Seq[Long]) = time({
