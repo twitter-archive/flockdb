d056790de2dc8cf6ad77ea6cbf005f7206878eb6
Raghavendra Prabhu <rvp@twitter.com>
2012-03-29 16:01:37 -0700

    Upgrade to gizzard 3.0.10

--- project/build/FlockDBProject.scala
+++ project/build/FlockDBProject.scala
@@ -10,7 +10,7 @@ with SubversionPublisher {
   override def filterScalaJars = false
   val scalaTools = "org.scala-lang" % "scala-compiler" % "2.8.1"
 
-  val gizzard   = "com.twitter" % "gizzard" % "3.0.8-SNAPSHOT-stuhood"
+  val gizzard   = "com.twitter" % "gizzard" % "3.0.10"
 
   val asm       = "asm"                     % "asm"          % "1.5.3" % "test"
   val cglib     = "cglib"                   % "cglib"        % "2.2"   % "test"
